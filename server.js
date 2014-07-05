9059baca7e63fa44a0f09e80401ad11a43feba3f
Michael Wooster <woosterm@ho-dgtlmrk-mac.local>
2014-07-05 17:08:24 -0400

    Added more setup files, also set up node to use handlebars

--- server.js
+++ server.js
@@ -1,4 +1,20 @@
 var express = require("express");
 var http = require("http");
 var app = express();
+var handlebars = require("express3-handlebars");
 
+app.set("port", process.env.PORT || 8000);
+app.set("views", "./views");
+app.engine("hbs", handlebars);
+app.set("view engine", "hbs");
+app.get("/", function(res, res){
+	res.render("index", {
+		title:"Base Social User Management System", 
+		keywords:"Social User Management",
+		description:"This is a base platform to be used in developmental projects that would require a user managment system"
+	});
+});
+
+http.createServer(app).listen(app.get("port"), function(){
+	console.log("Express Server listening on port " + app.get("port"));
+});
