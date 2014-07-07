e1ecec95275f9db52f3c364021394e5b171d51a3
unknown <RootsG@HO-ROOTSG-7L.simon.com>
2014-07-07 13:51:13 -0400

    Added Login, change passord, forgot password and register methods to Account model

--- models/Account.js
+++ models/Account.js
@@ -17,5 +17,76 @@ models.exports = function(config, mongoose, nodemailer){
 		biography: {type: String}
 	});
 
-	var Account = mongoose.model('')
+	var Account = mongoose.model('Account', AccountSchema);
+	var registerCallback = function(err){
+		if(err){
+			return console.log(err);
+		}
+		return console.log("Account has been created");
+	}
+
+	var changePassword = function(accountId, newPassword){
+		var shaShum = crypto.createHash('sha256');
+		shaShum.update(newPassword);
+		var hashedPassword = shaShum.digest('hex');
+		Account.update({_id: accountId}, {$set: {password: newPassword}}, {upsert: false}, function changeCallback(err){
+			console.log("Change password done for account " + accountId);
+		});
+	};
+
+	var forgotPassword = function(email, resetPasswordUrl, callback){
+		var user = Account.findOne({email:email}, function findAccount(err, doc){
+			if(err){
+				callback(false);
+			} else {
+				var smtpTransport = nodemailer.createTransport('SMTP', config.mail);
+				resetPasswordUrl += '?account=' + doc._id;
+				smtpTransport.sendMail({
+					from: 'groots@squishdesigns.com',
+					to: doc.email,
+					subject: 'A Message From George',
+					text: 'Email me back if you recieved this email.'
+				}, function forgotPasswordResult(err){
+					if(err){
+						callback(false);
+					} else {
+						callback(true);
+					}
+				});
+			}
+		});
+	};
+
+	var login = function(email, password, callback){
+		var shaShum = crypto.createHash('sha256');
+		shaShum.update(password);
+		Account.findOne({email:email, password:shaShum.digest('hex')}, function(err, doc){
+			callback(null!=doc);
+		});
+	};
+
+	var register = function(email, password, firstName, lastName){
+		var shaShum = crypto.createHash('sha256');
+		shaShum.update(password);
+
+		console.log("registering email " + email);
+		var user = new Account ({
+			email: email,
+			name: {
+				firstName: firstName,
+				lastName: lastName
+			},
+			password: shaShum.digest('hex')
+		});
+		user.save(registerCallback);
+		console.log("Save command was sent");
+	}
+
+	return {
+		register: register,
+		forgotPassword: forgotPassword,
+		changePassword: changePassword,
+		login: login,
+		Account: Acount
+	}
 }
